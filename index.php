cf9d0094921877634f398819b2456e2e75906659
mpatilShipco <mpatil@shipco.com>
2020-10-09 19:44:50 +0530

    Feature Comment - Fourth Commit after taking locally modified changes

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 echo 'Index.php';//commit One
 echo 'Index.php';//commit Two
 echo 'Index.php';//locally modified for production issue
+echo 'Index.php';//Git Pull locally modified changes and then commit
 
 
 ?>
